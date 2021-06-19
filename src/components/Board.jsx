6008f7bd05cc7f331d83078142edb713529ff7aa
ebenezer <e.kankam95@hotmail.com>
2021-06-19 17:32:51 -0500

    initial commit

---
+++ src/components/Board.jsx
@@ -0,0 +1,23 @@
+const Board = () => {
+    return (
+        <div>
+            <div>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+            <div>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+            <div>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+        </div>
+    )
+}
+
+export default Board
