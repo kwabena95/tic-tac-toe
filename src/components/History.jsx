f11353bfea83467234b9b5509e15ded5236191a3
ebenezer <e.kankam95@hotmail.com>
2021-06-19 20:12:19 -0500

    styled the entire app

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,21 +1,23 @@
 const History = ({ history, moveTo, currentMove }) => {
     return (
-        <ul>
-            {
-                history.map((_, move) => (
-                    <li key={move}>
-                        <button
-                            type='button'
-                            onClick={() => moveTo(move)}
-                            style={{ fontWeight: move === currentMove ? 'bold' : 'normal' }}
-                        >
-                            {move === 0 ? 'Go to game start' : `Go to move #${move}`}
-                        </button>
-                    </li>
+        <div className="history-wrapper">
+            <ul className='history'>
+                {
+                    history.map((_, move) => (
+                        <li key={move}>
+                            <button
+                                type='button'
+                                onClick={() => moveTo(move)}
+                                className={`btn-move ${move === currentMove ? 'active' : ''}`}
+                            >
+                                {move === 0 ? 'Go to game start' : `Go to move #${move}`}
+                            </button>
+                        </li>
 
-                ))
-            }
-        </ul>
+                    ))
+                }
+            </ul>
+        </div>
     )
 }
 
